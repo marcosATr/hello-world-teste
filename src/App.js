a2ab08f551852b716d27f57460c0ef6d53789e4e
marcosATr <marcos.trannin@gmail.com>
2022-05-20 09:11:57 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [res, setRes] = useState({ message: "" });
+  useEffect(() => {
+    const getdata = async () => {
+      const result = await fetch(`https://localhost:4000/hello`).then((res) => res.json);
+      setRes(result)
+    };
+    getdata()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       aqui: {res}
       </header>
     </div>
   );
